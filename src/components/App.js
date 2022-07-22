0abb43a850bd48cd403bf3dc08331693dedcaca1
Tathagat <tathagat1081@gmail.com>
2022-07-22 15:15:27 +0530

    third commit

--- src/components/App.js
+++ src/components/App.js
@@ -63,9 +63,6 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             Ethereum Wallet
           </a>
@@ -75,9 +72,6 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto" style={{ width: "500px" }}>
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
                 >
                   <img src={ethLogo} width="150" />
                 </a>
